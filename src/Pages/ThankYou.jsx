2cb66426f84ec5d7ba2bfabe55879adfbceffc70
tangentveil <anirudha0789@gmail.com>
2024-11-22 21:35:33 +0530

    logo added

--- src/Pages/ThankYou.jsx
+++ src/Pages/ThankYou.jsx
@@ -3,7 +3,7 @@ const ThankYou = () => {
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <div className="text-center">
         <h1 className="text-3xl font-bold text-green-500">Form Submitted Successfully!</h1>
-        <p className="mt-4 text-gray-600">Thank you for getting in touch. We will contact you shortly.</p>
+        <p className="mt-4 text-gray-600">Thank you for getting in touch. We will contact you 12hrs.</p>
         <a
           href="/"
           className="mt-6 inline-block bg-green-500 text-white px-6 py-2 rounded-md hover:bg-green-600"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
+
+
 const Navbar = () => {
   return (
-    <header className="shadow-lg h-16 sticky top-0">
-      <h1>Navbar</h1>
+    <header className="shadow-lg h-16 sticky top-0 bg-white z-50">
+      <img src="https://www.geeksofgurukul.com/assets/logo-50eabf9c.png" alt="Logo" className="h-16 w-36" />
     </header>
   );
 };
